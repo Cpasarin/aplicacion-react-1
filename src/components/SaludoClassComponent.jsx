9eaec91711bf219be76d93d9adf7282053d7dc1e
Cpasarin <128656950+Cpasarin@users.noreply.github.com>
2023-07-06 21:29:05 -0400

    clase2

---
+++ src/components/SaludoClassComponent.jsx
@@ -0,0 +1,8 @@
+class SaludoClassComponent extends React.Component{
+    render(){
+        return <h1> hola 👍🙈🐫🤲🏿 {this.props.username} Como estas? </h1>
+    }
+}
+
+
+export default SaludoClassComponent;
--- src/components/saludo.jsx
+++ src/components/saludo.jsx
@@ -3,3 +3,4 @@ const Saludos = (props) => {
 }
 
 export default Saludos;
+
